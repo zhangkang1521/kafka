dfeff9ea407cf987eccce77c12ca1c0abdba0553
zhangkang <zhangkang@lvmama.com>
2021-02-05 09:48:31 +0800

    单元测试完善

--- clients/src/test/java/org/zhangkang/test/ConsumerTest.java
+++ clients/src/test/java/org/zhangkang/test/ConsumerTest.java
@@ -5,29 +5,39 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.Properties;
 
 public class ConsumerTest {
 
-	@Test
-	public void receive() {
+	private KafkaConsumer consumer;
+
+	private static Logger logger = LoggerFactory.getLogger(ProducerTest.class);
+
+	@Before
+	public void before() {
 		Properties props = new Properties();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-1"); // 一个消息只会通知组内的1个消费者
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-		KafkaConsumer consumer = new KafkaConsumer<>(props);
+//		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // 默认自动提交
+		consumer = new KafkaConsumer<>(props);
+	}
+
+	@Test
+	public void receive() {
 		consumer.subscribe(Arrays.asList("test", "demo"));
 		while(true) {
 			ConsumerRecords<String, String> records = consumer.poll(1000);
 			for (ConsumerRecord<String, String> record : records) {
-				System.out.println("收到消息：" + record.value());
+				logger.info("收到消息：{}", record);
 			}
-			consumer.commitSync();
 		}
 	}
 }
--- clients/src/test/java/org/zhangkang/test/ProducerTest.java
+++ clients/src/test/java/org/zhangkang/test/ProducerTest.java
@@ -1,32 +1,42 @@
 package org.zhangkang.test;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ProducerTest {
 
-	@Test
-	public void testSend() {
+	Producer<String, String> kafkaProducer;
+
+	private static Logger logger = LoggerFactory.getLogger(ProducerTest.class);
+
+	@Before
+	public void before() {
 		Properties props = new Properties();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		// key序列化（必须）
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 		// value序列化（必须）
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-
 		// 0：无需确认 1：leader确认收到 all:所有节点确认收到 默认1,默认值在ProducerConfig静态代码块中
 //		props.put(ProducerConfig.ACKS_CONFIG, "1");
+		kafkaProducer = new KafkaProducer<>(props);
+	}
 
-		Producer<String, String> kafkaProducer = new KafkaProducer<>(props);
-
+	@Test
+	public void testSend() {
 		// 往 RecordAccumulator 放，单独的线程 Sender 读取并发送
-		kafkaProducer.send(new ProducerRecord<>("test", "hello,world"));
-
+		kafkaProducer.send(new ProducerRecord<>("test", "hello,world"), (RecordMetadata metadata, Exception exception) -> {
+			if (exception == null) {
+				logger.info("发送成功, {}", metadata);
+			} else {
+				logger.error("发送失败", exception);
+			}
+		});
 		kafkaProducer.close();
 	}
 
